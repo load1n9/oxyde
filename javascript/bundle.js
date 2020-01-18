2804ae0447f7a8653393a928ba4342439168bbf1
loading <49134864+load1n9@users.noreply.github.com>
2020-01-18 16:45:51 -0500

    Create bundle.js

---
+++ javascript/bundle.js
@@ -0,0 +1,44 @@
+
+
+console.log(`%c             $$$$$$$$              $$$$$$
+           m$$$$$$$$$          m$"""" "$$m             
+           $$$$$$$$$$         $" m$$$$m""$m           
+          mm$$$$$$$$$$$$$$$"" $     "$$mm$$           
+          m""        """$$$$$mm      "$$$$$           
+      mmm$$m$$m$$mmmmm   """"$$$"    m$$$"            
+    ""$m   $$$""""""$$$mm            $$$$m            
+       $                ""           mm$$$$           
+     mm"                      mm$$$$$$$$$$$           
+     $                      m$$$$""    ""$$$          
+     $                      $$""       m "$$m         
+    $                      """   mm$$$$$$$$$$         
+    $                          m$$$$$""""$$$"         
+     $  mm    mm               $$"       "$$          
+     $ $"$$  $"$$                         "$""""      
+      $"m"   "m$"                           m""""     
+     m$ m"""m                                "mmm""   
+    $ mmmm                                   ""       
+ mmm$$""""""$$                                m""""$  
+m$$"  mm$$$$$$$                                mm     
+$$  $$$$$$$$$$$$         mmm                "$m "$m   
+"$$$$$$$$$$$$$$$         m$m"                m     "  
+ ""$$$$$$$$$$$$"       m$$$$                 ""mm     
+   $""$$$$$""         m$$$$                m mmm "    
+   "m              m$$$$$$            mm$mmmm  ""     
+    ""mmm     mmm$$""$$$"            $$""m   "        
+         """""   "$mm$"     m        $$               
+                m         m$"        $$               
+                "$$$$$mm$""          ""               
+                   """"                                  
+`, "font-family:monospace");
+console.log(`code made by @loading...#9877`)
+
+
+function compile() {
+   var input = document.getElementById("input").value;
+   var output  = document.getElementById("output");
+   var template = document.getElementById("templates");
+   output.innerHTML = input;
+   setTimeout(compile, 100);
+   
+ }

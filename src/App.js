da60b43be01ea9dd400d39ee5a92df687a6593d4
loading <49134864+load1n9@users.noreply.github.com>
2020-11-09 15:58:56 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,59 +2,68 @@ import React from "react";
 import MonacoEditor from "react-monaco-editor";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      code: `
+    constructor(props) {
+        super(props);
+        this.state = {
+            code: `
       function test() {
           console.log("hi")
       }
       `,
-      theme: "vs-dark"
+            theme: "vs-dark",
+            language: "javascript"
+        };
+    }
+    editorDidMount = (editor, monaco) => {
+        console.log("editorDidMount", editor);
+        editor.focus();
     };
-  }
-  editorDidMount = (editor, monaco) => {
-    console.log("editorDidMount", editor);
-    editor.focus();
-  };
-  onChange = (newValue, e) => {
-    console.log("onChange", newValue, e);
-  };
-  setTheme = () => {
-    this.setState({
-      theme: this.state.theme === "vs-light" ? "vs-dark" : "vs-light"
-    });
-  };
+    onChange = (newValue, e) => {
+        console.log("onChange", newValue, e);
+    };
+    setTheme = () => {
+        this.setState({
+            theme: this.state.theme === "vs-light" ? "vs-dark" : "vs-light"
+        });
+    };
+    setLanguage = () => {
+        this.setState({
+            language: this.state.language === "javascript" ? "python" : "javascript"
+        });
+    };
 
-  render() {
-    const { code, theme } = this.state;
-    const options = {
-      selectOnLineNumbers: true,
-      roundedSelection: true,
-      readOnly: false,
-      cursorStyle: "line",
-      automaticLayout: true,
-      cursorBlinking: "blink"
-    };
-    return (
-      <div class={theme === "vs-light" ? "light" : "dark"}>
-        <div>
-          <ul className="topElement" onClick={this.setTheme}>
-            {theme === "vs-light" ? "🌙" : "☀️"}
-          </ul>
-        </div>
-        <MonacoEditor
-          height="800"
-          language="javascript"
-          value={code}
-          options={options}
-          onChange={this.onChange}
-          editorDidMount={this.editorDidMount}
-          theme={theme}
-        />
-      </div>
-    );
-  }
+    render() {
+        const { code, theme, language } = this.state;
+        const options = {
+            selectOnLineNumbers: true,
+            roundedSelection: true,
+            readOnly: false,
+            cursorStyle: "line",
+            automaticLayout: true,
+            cursorBlinking: "blink"
+        };
+        return (
+            <div class={theme === "vs-light" ? "light" : "dark"}>
+                <div>
+                    <ul className="topElement" onClick={this.setTheme}>
+                        {theme === "vs-light" ? "🌙" : "☀️"}
+                    </ul>
+                    <ul className="topElement" onClick={this.setLanguage}>
+                        {language === "javascript" ? "🐍" : "📜"}
+                    </ul>
+                </div>
+                <MonacoEditor
+                    height="800"
+                    language={language}
+                    value={code}
+                    options={options}
+                    onChange={this.onChange}
+                    editorDidMount={this.editorDidMount}
+                    theme={theme}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;

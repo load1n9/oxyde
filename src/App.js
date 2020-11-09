16f6b0ca3de877a782a831cd36e5a64f76a18267
loading <dsrebnik@gmail.com>
2020-11-09 14:52:01 -0500

    rewritten

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React from "react";
+import MonacoEditor from "react-monaco-editor";
+import { Octokit } from "@octokit/rest";
+const octokit = new Octokit({
+  auth: "mypersonalaccesstoken123"
+});
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      code: `
+      function test() {
+          console.log("hi")
+      }
+      `,
+      theme: "vs-dark"
+    };
+  }
+  editorDidMount = (editor, monaco) => {
+    console.log("editorDidMount", editor);
+    editor.focus();
+  };
+  onChange = (newValue, e) => {
+    console.log("onChange", newValue, e);
+  };
+  setTheme = () => {
+    this.setState({
+      theme: this.state.theme === "vs-light" ? "vs-dark" : "vs-light"
+    });
+  };
+
+  render() {
+    const { code, theme } = this.state;
+    const options = {
+      selectOnLineNumbers: true,
+      roundedSelection: true,
+      readOnly: false,
+      cursorStyle: "line",
+      automaticLayout: true,
+      cursorBlinking: "blink"
+    };
+    return (
+      <div class={theme === "vs-light" ? "light" : "dark"}>
+        <div>
+          <ul className="topElement" onClick={this.setTheme}>
+            {theme === "vs-light" ? "🌙" : "☀️"}
+          </ul>
+        </div>
+        <MonacoEditor
+          height="800"
+          language="javascript"
+          value={code}
+          options={options}
+          onChange={this.onChange}
+          editorDidMount={this.editorDidMount}
+          theme={theme}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;

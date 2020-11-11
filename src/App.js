f6db8d34d806fb619adf5bb7819757eb4a9fcddd
loading <dsrebnik@gmail.com>
2020-11-11 14:50:44 +0000

    fixed this

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ class App extends React.Component {
             .then((data) => this.setState({ code: data }));
     }
     UploadParams = () => {
-        alert(`https://oxyde-ide.vercel.app/?code=${btoa(this.state.code)}`)
+        prompt("copy url to share: "`https://oxyde-ide.vercel.app/?code=${btoa(this.state.code)}`)
     }
 
     render() {
